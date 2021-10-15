6f5370be7829a29f0e0f9c5b2d21eee2a8294275
Vincent DARON <vda@dfakto.com>
2021-10-15 07:56:27 +0200

    Add some more documentation

--- dFakto.Rest.AspNetCore.Mvc/Requests/ResourceCollectionRequest.cs
+++ dFakto.Rest.AspNetCore.Mvc/Requests/ResourceCollectionRequest.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace dFakto.Rest.AspNetCore.Mvc.Requests
 {
+    /// <summary>
+    /// Base Request while Requesting collection of Resources
+    /// </summary>
     public class ResourceCollectionRequest : ResourceRequest
     {
         /// <summary>
@@ -23,6 +26,12 @@ namespace dFakto.Rest.AspNetCore.Mvc.Requests
         [FromQuery(Name = "limit")]
         public int? Limit { get; set; }
 
+        /// <summary>
+        /// Simple Filter that needs to be applied on the collection items.
+        /// Format is [PropertyName] [Operator] [Value]. For a complete list of available operators, see <see cref="Filter"/>
+        /// <example>Name = Bob</example>
+        /// <example>BirthDate gt 1990-10-01</example>
+        /// </summary>
         [FromQuery(Name = "filter")]
         [ModelBinder(typeof(FilterModelBinder))]
         public Filter Filter { get; set; }
--- dFakto.Rest.AspNetCore.Mvc/Requests/ResourceRequest.cs
+++ dFakto.Rest.AspNetCore.Mvc/Requests/ResourceRequest.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace dFakto.Rest.AspNetCore.Mvc.Requests
 {
+    /// <summary>
+    /// Base Request object while requesting Ressources
+    /// </summary>
     public class ResourceRequest
     {
         /// <summary>
         /// List of links that need to be expanded.
-        /// The format can be a link name or an embedded resource name followed by the link name (embedded.link)
+        /// The format can be a link name or an embedded resource name followed by the link name (embedded.link).
         /// </summary>
         [FromQuery(Name = "expand")]
         public string[] Expand { get; set; } = Array.Empty<string>();
