b4aa8695f908b18950670e55dfab8621d46871ba
Vincent DARON <vda@dfakto.com>
2023-06-01 07:47:36 +0200

    Add some extensions methods over ISIngleOrList when T is an IResource

--- dFakto.Rest.Abstractions/ISingleOrList.cs
+++ dFakto.Rest.Abstractions/ISingleOrList.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace dFakto.Rest.Abstractions;
 
@@ -8,4 +10,26 @@ public interface ISingleOrList<out T>
     IEnumerable<T> Values { get; }
     T Value { get; }
     bool SingleValued { get; }    
+}
+
+public static class SingleOrListExtensions
+{
+    public static IResource? FirstOrDefault<T>(
+        this ISingleOrList<IResource> resources,
+        Func<T,bool> filter)
+    {
+        return resources.Values.FirstOrDefault(x => filter(x.As<T>()));
+    }
+    public static IResource First<T>(
+        this ISingleOrList<IResource> resources,
+        Func<T,bool> filter)
+    {
+        return resources.Values.First(x => filter(x.As<T>()));
+    }
+    public static IEnumerable<IResource> Where<T>(
+        this ISingleOrList<IResource> resources,
+        Func<T,bool> filter)
+    {
+        return resources.Values.Where(x => filter(x.As<T>()));
+    }
 }
